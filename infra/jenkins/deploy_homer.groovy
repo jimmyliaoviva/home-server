8b9f36823811bbde49211e62584a8f02a1906ce1
jimmyliaoviva <jimmy.liao86@gmail.com>
2024-01-20 22:16:44 +0800

    [JT-7] add pipeline for testing

--- infra/jenkins/deploy_homer.groovy
+++ infra/jenkins/deploy_homer.groovy
@@ -1,6 +1,14 @@
 pipeline {
     agent any
     stages {
+        stage('checkout') {
+            steps {
+                sh 'mkdir -p home-server'
+                dir ('home-server') {
+                    git branch: 'master', credentialsId: 'jimmyliaoviva', url: 'git@github.com:jimmyliaoviva/home-server.git'
+                }
+            }
+        }
         stage('run deploy playbook') {
             steps {
                 sh 'ansible-playbook -i infra/jenkins/inventory infra/ansible/deploy_homer.yml'
