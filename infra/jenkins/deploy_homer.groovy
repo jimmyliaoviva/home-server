40289d634995581604e6b4608757fb34a6f06d85
jimmyliaoviva <jimmy.liao86@gmail.com>
2024-01-20 23:14:28 +0800

    disable strict host checking

--- infra/jenkins/deploy_homer.groovy
+++ infra/jenkins/deploy_homer.groovy
@@ -4,8 +4,14 @@ pipeline {
         stage('checkout') {
             steps {
                 sh 'mkdir -p home-server'
+                sh '''
+                    mkdir -p ~/.ssh
+                    echo "Host *" > ~/.ssh/config
+                    echo "  StrictHostKeyChecking no" >> ~/.ssh/config
+                    chmod 600 ~/.ssh/config
+                    '''
                 dir ('home-server') {
-                    git branch: 'master', credentialsId: 'github', url: 'git@github.com:jimmyliaoviva/home-server.git'
+                    git branch: 'main', credentialsId: 'github', url: 'git@github.com:jimmyliaoviva/home-server.git'
                 }
             }
         }
