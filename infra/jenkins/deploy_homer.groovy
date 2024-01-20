2898402768a7dae34fc0d9975cb4e81af8e54b55
jimmyliaoviva <jimmy.liao86@gmail.com>
2024-01-20 23:16:41 +0800

    revert

--- infra/jenkins/deploy_homer.groovy
+++ infra/jenkins/deploy_homer.groovy
@@ -4,12 +4,6 @@ pipeline {
         stage('checkout') {
             steps {
                 sh 'mkdir -p home-server'
-                sh '''
-                    mkdir -p ~/.ssh
-                    echo "Host *" > ~/.ssh/config
-                    echo "  StrictHostKeyChecking no" >> ~/.ssh/config
-                    chmod 600 ~/.ssh/config
-                    '''
                 dir ('home-server') {
                     git branch: 'main', credentialsId: 'github', url: 'git@github.com:jimmyliaoviva/home-server.git'
                 }
