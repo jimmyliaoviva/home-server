edaec5bc302749d31eec357d10947409b7c8ea9b
jimmyliaoviva <jimmy.liao86@gmail.com>
2024-04-28 23:03:13 +0800

    add finmind

---
+++ infra/jenkins/deploy_finmind.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+    stages {
+        stage('checkout') {
+            steps {
+                sh 'mkdir -p finmind'
+                dir ('finmind') {
+                    git branch: 'master', credentialsId: 'github', url: 'git@github.com:jimmyliaoviva/home-server.git'
+                }
+            }
+        }
+        stage('run deploy playbook') {
+            steps {
+                script {
+                    dir ('home-server/infra/ansible') {
+                        sshagent(credentials: ['portainer2']) {
+                                withCredentials([string(credentialsId: 'portainer_password', variable: 'PASS')]) {
+                            sh '''
+                                echo "$SSH_KEY" > temp_key.pem
+                                chmod 600 temp_key.pem
+                            '''
+                            sh 'ansible-playbook -i inventory deploy-finmind-playbook.yml \ 
+                                -e "ansible_become_pass=${PASS}" \
+                                -e "github_key=${temp_key.pem}" \'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
